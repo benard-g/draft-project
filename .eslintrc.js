efc41cdb6df703b64a60c42f72a71e01b0f2b0d4
Guillaume Benard <guillaume@benard.io>
2020-10-16 15:43:27 +0200

    Bootstrap project (#1)
    Create a (blank) project structure using yarn workspaces.
    
    * Create project lib/backend-commons
    * Create project backend/ms-gateway
    * Create project frontend/web-app
    * Add CI

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    jest: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['simple-import-sort'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'all', argsIgnorePattern: '^_' },
+    ],
+    'no-console': 'warn',
+    'no-return-await': 'error',
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+  },
+};
